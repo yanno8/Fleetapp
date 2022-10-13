7347d56b4dbbb1e443bdd6e46605f1cfaa216b3f
Yanno8 <yannesonwa@yahoo.com>
2022-10-13 11:55:25 +0100

    Ajout des elements du projet.

---
+++ src/main/java/com/kindsonthegenius/Fleetapp/FleetappApplication.java
@@ -0,0 +1,13 @@
+package com.kindsonthegenius.Fleetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetappApplication.class, args);
+	}
+
+}
